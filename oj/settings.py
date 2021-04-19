fd5f09918018ed8c3a9a87e90c3aa7c3975ec1d0
e <504428538@qq.com>
2021-04-19 22:54:40 +0800

    修改后端对应指向前端的打包文件路径，使得系统页面可以正常显示，但是仍有部分图标无法正常加载

--- oj/settings.py
+++ oj/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/public/'
+STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'account.User'
 
